7315161af3ddd46512ff89f0f4c267fb1a18b12e
Brandon Durand <ptco.dev@gmail.com>
2024-02-23 14:45:22 -0600

    Final

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const keyboard = document.querySelector('#qwerty');
-const phrase = document.querySelector('#phrase');
+const overlay = document.querySelector('#overlay');
+const phrase = document.querySelector('#phrase ul');
 let missed = 0;
-
-document.querySelector('.btn__reset').addEventListener('click', ()=>{
-    document.querySelector('#overlay').style.display = 'none';
-;});
 
 const phrases = [
     'A piece of cake',
@@ -22,8 +19,6 @@ function getRandomPhraseAsArray(arr) {
     return arr[Random_Number].split("");
 }
 
-const phraseArray = getRandomPhraseAsArray(phrases);
-
 function addPhraseToDisplay(arr) {
     arr.map((character)=>{
         const li = document.createElement('li');
@@ -35,27 +30,103 @@ function addPhraseToDisplay(arr) {
     })
 }
 
-addPhraseToDisplay(phraseArray);
-
 function checkLetter(letter) {
+
+    let correct = '';
 
     phraseArray.map((character, index)=>{
         if(character.toLowerCase() == letter.textContent){
             phrase.querySelectorAll('li')[index].className = 'show';
 
-            let correct = character.textContent;
-
-            return correct;
+             correct = character;
 
         } else {
 
             return null;
         }
     })
+
+    return correct;
 }
 
-keyboard.addEventListener('click', (e)=>{
-    e.target.setAttribute('disabled', "");
+function checkWin(){
 
-    const letterFound = checkLetter(e.target);
+    const letter_count = document.querySelectorAll('.letter').length;
+
+    const show_count = document.querySelectorAll('show').length;
+
+    function createElement(tag, property, value){
+
+        const element = document.createElement(tag);
+
+        property.map((prop, index)=>{
+            element[prop] = value[index];
+        })
+        
+
+        return element;
+
+    }
+
+    function showWinCondition(status){
+
+        overlay
+
+        overlay.className = status;
+        overlay.style.display = 'flex';
+
+        const conditionMessage = createElement('h4', ['textContent'], [`You ${status} !`]);
+
+        const retryButton = createElement('button', ['textContent', 'className'], ['Play Again!', 'btn__retry'])
+
+        overlay.lastElementChild.remove();
+
+        overlay.appendChild(conditionMessage);
+        overlay.appendChild(retryButton);
+
+    }
+
+    if(missed >= 5){
+        showWinCondition('lose');
+    }
+    else if (letter_count === show_count) {
+        showWinCondition('win');
+    }
+    
+
+}
+
+const phraseArray = getRandomPhraseAsArray(phrases);
+addPhraseToDisplay(phraseArray);
+
+
+keyboard.addEventListener('click', (e)=>{
+
+    if(e.target.tagName === 'BUTTON') {
+
+        const button = e.target;
+
+        button.setAttribute('disabled', "");
+        button.style.backgroundColor = 'green';
+
+        const letterFound = checkLetter(button);
+        if(!letterFound) {
+            button.style.backgroundColor = 'red';
+            missed++;
+            const Tries = document.querySelectorAll('.tries img');
+            Tries[missed - 1].src = "./images/lostHeart.png"
+        }
+
+    }
+
+    checkWin()
 })
+
+overlay.addEventListener('click', (e)=>{
+    if (e.target.className === 'btn__retry'){
+        location.reload();
+    }
+    document.querySelector('#overlay').style.display = 'none';
+});
+
+
